fbd0f4a9a21b651876d82f5fed64480e9707c021
IlgarMammadov <iilqarmemmedov099@gmail.com>
2020-01-20 22:55:00 +0400

    first commit

---
+++ src/test/java/com/bankapi/microservices/gateway/eurekazuulservice/EurekaZuulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankapi.microservices.gateway.eurekazuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaZuulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
